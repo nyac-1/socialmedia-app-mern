7b61ee9b2bba91ac31b7462cc7db0013518e878a
nyac-1 <routusampath@yahoo.com>
2021-10-05 15:41:50 +0530

    Node basics completed

--- prereq/node_basic/app.js
+++ prereq/node_basic/app.js
@@ -1,4 +1,22 @@
 const helpers = require('./helpers.js');
+const express = require('express');
+// const {sum} = require('./helpers.js');
+const http = require('http');
+
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send("how are you ?");
+
+})
+
+app.listen(3000);
+
+// const server = http.createServer((req, res)=>{
+//     res.end("Hey dude how yu doing");
+// })
+
+// server.listen(3000);
 
 var totes = helpers.sum(10,20);
 console.log(totes);
