f879279ce149fa52c140bd0600c2744a2e96ab0e
nyac-1 <routusampath@yahoo.com>
2021-10-12 10:53:24 +0530

    user signup validation update

--- node_react/node_api/routes/auth.js
+++ node_react/node_api/routes/auth.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const authController = require('../controllers/auth.js');
-
-
+const validator = require('../validator');
 
 const router = express.Router();
 
-router.post("/signup",authController.signup);
-
-
+router.post("/signup",validator.signupValidator,authController.signup);
 
 module.exports = router;
--- node_react/node_api/validator/index.js
+++ node_react/node_api/validator/index.js
@@ -19,3 +19,30 @@ exports.createPostValidator = (req, res, next)=>{
     }
     next();
 };
+
+exports.signupValidator = (req, res, next)=>{
+    
+    req.check('name','Enter a name').notEmpty();
+    req.check('name','Length range is from 4 to 20 characters').isLength({
+        min: 4,
+        max: 20
+    });
+
+    req.check('email','Enter an email').notEmpty();
+    req.check('email','Format your email properly').isEmail();
+
+    req.check('password','Enter a password').notEmpty();
+    req.check('password','Length range is from 4 to 50 characters').isLength({
+        min: 4,
+        max: 50
+    });
+    req.check('password').matches(/\d/).withMessage('Must contain atleast one number');
+    
+    const errors = req.validationErrors();
+    if(errors){
+        const firstError = errors[0]['msg']
+        console.log(firstError);
+        return res.status(400).json({error: firstError});
+    }
+    next();
+}
